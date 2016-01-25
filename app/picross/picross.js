5c991a31c732880916bbf0d024fd1ad20eaa72ce
Richard Dijk <microvirus.multiplying@gmail.com>
2016-01-25 16:02:40 +0100

    First commit. Randomly generates a picross and displays it in a grid.
    The CSS for the grid is botchd, because I had an earlier version without the headers, for which the CSS still is.

---
+++ app/picross/picross.js
@@ -0,0 +1,175 @@
+'use strict';
+
+angular.module('picross.picross', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/picross', {
+        templateUrl: 'picross/picross.html',
+        controller: 'PicrossCtrl'
+    });
+}])
+
+.controller('PicrossCtrl', ['$scope', function($scope) {
+    // Playing field values
+    const Unticked = 0, Ticked = 1, Crossed = 2;
+
+    // Define picross
+    $scope.picross = generatePicross(15, 15);
+
+    // Calculate view quantities
+    var topHeaderSize = Math.max.apply(Math, $scope.picross.columnHints.map(function (arr) {return arr.length;}));
+    var leftHeaderSize = Math.max.apply(Math, $scope.picross.rowHints.map(function (arr) {return arr.length;}));
+
+    // Create top header (basically a transpose with zero-fill)
+    $scope.topHeaderRows = rangeArray(0, topHeaderSize).map(
+        getTopHeaderRow.bind(null, $scope.picross.width, $scope.picross.columnHints, topHeaderSize));
+    $scope.leftHeaderRows = rangeArray(0, $scope.picross.height).map(
+        function (hints, leftHeaderSize, row) {
+            var hint = hints[row];
+            var pad = leftHeaderSize - hint.length;
+            return createArray(pad, "").concat(hint);
+        }.bind(null, $scope.picross.rowHints, leftHeaderSize));
+
+    function getTopHeaderRow(width, hints, topHeaderSize, headerRow)
+    {
+        var header = [];
+        for (var col = 0; col < width; col++)
+        {
+            var hint = hints[col];
+            var index = headerRow - (topHeaderSize - hint.length);
+            header.push(index >= 0 ? hint[index] : "");
+        }
+        return header;
+    }
+
+    // Testing the picross by filling rows with random numbers; need to be become objects (arrays, probably) later on.
+    //var picross = $scope.picross;
+    //picross.rows = Array.apply(null, Array(picross.width)).map(function () {return Math.floor(Math.random() * (picross.width+1));});
+    //picross.columns = Array.apply(null, Array(picross.height)).map(function () {return Math.floor(Math.random() * (picross.height+1));});
+
+
+
+    ///
+    /// Picross generation
+    ///
+
+    function generatePicross(width, height)
+    {
+        return new Picross(width, height, generateImage(width, height));
+    }
+    function generateImage(width, height, fillingFactorHint)
+    {
+        fillingFactorHint = fillingFactorHint || 0.5;
+
+        var filling = 0;
+        var image = Array.apply(null, Array(width * height)).map(function () {return false;});
+        for (var i = 0; i < fillingFactorHint * width * height; i++) {
+            var row = randomInt(0, height - 1), col = randomInt(0, width - 1);
+            if ( ! image[row * width + col]) filling++;
+            image[row * width + col] = true;
+        }
+        console.log("Generated new Picross image: filling factor is " + filling/(width*height) + "% (" + filling + " out of " + width*height + ")");
+
+        return image;
+    }
+
+
+    function columnHintFromImage(image, width, height, col)
+    {
+        return hintFromImageScan(image, width, height, col, 0, 0, 1);
+    }
+    function rowHintFromImage(image, width, height, row)
+    {
+        return hintFromImageScan(image, width, height, 0, row, 1, 0);
+    }
+    // Create the row/column hint from the image. {colDelta: 1, rowDelta: 0} can be used for
+    // scanning a row and the reverse for scanning a column.
+    // Scanning starts at (col, row) and goes in the direction (colDelta, rowDelta).
+    function hintFromImageScan(image, width, height, col, row, colDelta, rowDelta)
+    {
+        var hint = [];
+        var currentBlockLength = 0;
+        for ( ; col < width && row < height; col += colDelta, row += rowDelta)
+        {
+            if (image[row * width + col])
+            {
+                currentBlockLength++;
+            }
+            else
+            {
+                if (currentBlockLength > 0)
+                    hint.push(currentBlockLength);
+                currentBlockLength = 0;
+            }
+        }
+        if (currentBlockLength > 0)
+            hint.push(currentBlockLength);
+        return hint;
+    }
+
+
+    ///
+    /// View functions
+    ///
+
+    $scope.getStyleForImage = function (row, col)
+    {
+        return $scope.picross.image[row * $scope.picross.width + col] ? 'field-ticked' : 'field-unticked';
+    }
+
+
+    ///
+    /// Picross object
+    ///
+
+    function Picross(width, height, image) {
+        this.width = width;
+        this.height = height;
+        this.image = image; // Source image
+        // The numbers of consecutive ticked boxes in the rows/columns.
+        this.columnHints = rangeArray(0, width).map(
+            function (col) {return columnHintFromImage(image, width, height, col);});
+        this.rowHints = rangeArray(0, height).map(
+            function (row) {return rowHintFromImage(image, width, height, row);});
+        // Playing field
+        this.field = createArray(width * height, Unticked);
+    }
+    Picross.prototype.getField = function (row,col) {
+        if (row < 0 || row >= height || col < 0 || col >= width)
+            throw "row/col out of range";
+
+        return this.field[row * width + col];
+    }
+    Picross.prototype.setField = function (row,col,val) {
+        if (row < 0 || row >= height || col < 0 || col >= width)
+            throw "row/col out of range";
+
+        this.field[row * width + col] = val;
+        return this;
+    }
+
+
+
+    ///
+    /// HELPER FUNCTIONS
+    ///
+
+    // Generates random number (using Math.random) in the range [min, max].
+    function randomInt(min, max)
+    {
+        return min + Math.floor(Math.random() * (max - min + 1));
+    }
+
+    // Generates a list [min, max) of max-min elements.
+    function rangeArray(min, max)
+    {
+        return Array.apply(null, Array(max-min)).map(function (_,i) {return min+i;});
+    }
+
+    // Create an array of size filled with val
+    function createArray(size, val)
+    {
+        return Array.apply(null, Array(size)).map(function () {return val;});
+    }
+
+}]);
